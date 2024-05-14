389db2076e562fde9397d4e8d4235b29898cc50f
born-A <93516595+born-A@users.noreply.github.com>
2024-05-14 09:57:25 +0900

    날짜, 타입 정리

---
+++ sql연습/06.타입.sql
@@ -0,0 +1,6 @@
+-- type
+-- 문자 : varchar, char, text, CLOB(Characcter Large OBject)
+-- 정수 : medium int, int(signed, integer), unsigned, big int 
+-- 실수 : float, double
+-- 시간 : date, datetime
+-- LOB : CLOB, BLOB(Binary Large OBject)
